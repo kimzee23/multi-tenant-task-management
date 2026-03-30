1b8de99e4845197def1c1cafa701a342a11aa64c
kimzee23 <kolajokimzee2020@gmail.com>
2026-03-30 04:17:31 +0100

    implement User model, service control

---
+++ src/main/java/com/indent/multitenanttodoapplication/infrastructure/adapter/input/rest/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.indent.multitenanttodoapplication.infrastructure.adapter.input.rest.controller;
+
+import com.indent.multitenanttodoapplication.application.ports.input.CreateUserUseCase;
+import com.indent.multitenanttodoapplication.domain.model.UserModel;
+import com.indent.multitenanttodoapplication.domain.model.enumType.UserRole;
+import com.indent.multitenanttodoapplication.infrastructure.adapter.input.rest.data.request.UserRequest;
+import com.indent.multitenanttodoapplication.infrastructure.adapter.input.rest.data.response.UserResponse;
+import com.indent.multitenanttodoapplication.infrastructure.adapter.output.persistence.mapper.UserMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+public class UserController {
+
+    private CreateUserUseCase useCase;
+
+    public UserController(CreateUserUseCase useCase) {
+        this.useCase = useCase;
+    }
+
+    @PostMapping
+    public UserResponse createUser(
+            @RequestHeader("X-Tenant-ID") String tenantId,
+            @RequestBody UserRequest request
+    )
+    {
+        UserModel user = useCase.createUser(
+                tenantId,
+                request.getEmail(),
+                UserRole.valueOf(request.getRole())
+        );
+        return UserMapper.toResponse(user);
+    }
+}
